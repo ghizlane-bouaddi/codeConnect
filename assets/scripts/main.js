a0764bdc431062788aba53870f12d137e50a74e8
n1o2h <aythmadnhylt@gmail.com>
2025-11-09 23:31:04 +0100

    ajout des fonction qui ajput le menue burger et function closeMenue qui ferme le menue burger

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+// le menue de navigation hamburger //
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", ()=>{
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
+});
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+hamburger.classList.remove("active");
+navMenu.classList.remove("active");
+}
