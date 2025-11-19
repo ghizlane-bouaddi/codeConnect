647dea6515c236016947c457a2d172e55c820ed2
makhfi03 <ghita.makhfi.21@gmail.com>
2025-11-19 11:57:58 +0100

    file html et javascript Mise en situation

---
+++ scriptMise.js
@@ -0,0 +1,11 @@
+// let saveStorage;
+// let ajout = document.getElementById("submitBtn").addEventListener("submit", () =>{
+//     saveStorage = localStorage.setItem("user", value);
+// });
+
+
+// let alert = document.getElementById("emptyMessage").addEventListener("", () =>{
+//     alert.display("none");
+// });
+
+
